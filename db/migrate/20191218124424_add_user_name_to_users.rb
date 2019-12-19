38576aee34166a9e03185494f581540c2c2a4854
samkhiem1 <samkhiem1@gmail.com>
2019-12-19 19:05:56 +0700

    can upgrade an member to admin

--- db/migrate/20191218124424_add_user_name_to_users.rb
+++ db/migrate/20191218124424_add_user_name_to_users.rb
@@ -1,7 +1,8 @@
 class AddUserNameToUsers < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :name, :string
     add_column :users, :phone, :string
-    add_column :users, :total_coi, :integer
-    add_column :users, :admin, :boolean
+    add_column :users, :total_coin, :integer
+    add_column :users, :admin, :boolean, default: false
   end
 end
