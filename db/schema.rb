5bea9fc14dbfc80bec291b7abdf1a59ac17d0f9e
samkhiem1 <samkhiem1@gmail.com>
2020-03-14 16:58:31 +0700

    category

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_104402) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "payments", force: :cascade do |t|
+ create_table "payments", force: :cascade do |t|
     t.integer "used_id"
     t.integer "buyer_id"
     t.integer "coin"
